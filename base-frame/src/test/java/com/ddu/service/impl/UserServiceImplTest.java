ded874986eea1f3227d29cc5ecbb0fd8924ecaa2
lvm <lvm@mkg-zncxzx3.maoye.local>
2017-03-29 19:49:51 +0800

    提交测试类

---
+++ base-frame/src/test/java/com/ddu/service/impl/UserServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.ddu.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ddu.entity.User;
+import com.ddu.service.UserService;
+
+public class UserServiceImplTest {
+
+	private UserService userService;
+    
+    /**
+     * 这个before方法在所有的测试方法之前执行，并且只执行一次
+     * 所有做Junit单元测试时一些初始化工作可以在这个方法里面进行
+     * 比如在before方法里面初始化ApplicationContext和userService
+     */
+    @Before
+    public void before(){
+        //使用"spring.xml"和"spring-mybatis.xml"这两个配置文件创建Spring上下文
+        ApplicationContext ac = new ClassPathXmlApplicationContext(new String[]{"spring.xml","spring-mybatis.xml"});
+        //从Spring容器中根据bean的id取出我们要使用的userService对象
+        userService = (UserService) ac.getBean("userService");
+    }
+    
+    @Test
+    public void testAddUser(){
+    	for (int i = 0; i < 10; i++) {
+    		User user = new User();
+            user.setName("name"+i);
+            user.setCreateTime(new Date());
+            userService.insert(user);
+		}
+    	List<User> list = userService.queryAll();
+    	for (User user : list) {
+			System.err.println(user.toString());
+		}
+    }
+
+}
